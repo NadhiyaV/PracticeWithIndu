07bb03cad73dc086a037d3480d79835cf8d5cc18
NadhiyaV <qspidersnadhiya@gmail.com>
2024-08-19 20:08:58 +0530

    new class created

---
+++ src/test/java/ContactsPackage/WorkWithJenkinsTest.java
@@ -0,0 +1,10 @@
+package ContactsPackage;
+
+import org.testng.annotations.Test;
+
+public class WorkWithJenkinsTest {
+@Test
+public void workwithjenkinstest() {
+	System.out.println("jenkins practice");
+}
+}
